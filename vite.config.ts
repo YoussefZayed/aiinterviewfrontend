f3c9c26b6337ab0e4daeb6b8a12c68501c3539a1
YoussefLaunchUp <youssef.Ah.zayed@gmail.com>
2024-06-03 02:08:27 -0500

    chore: Update vite.config.ts to include host parameter, set output directory, and ignore deprecation warnings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import path from "path";
+import path, { join } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+const srcRoot = join(__dirname, "src");
 
 export default defineConfig({
   plugins: [react()],
@@ -10,9 +11,12 @@ export default defineConfig({
     },
   },
   server: {
+    host: "0.0.0.0",
     port: process.env.PORT || 5173,
   },
   build: {
+    outDir: join(srcRoot, "/out"),
+    emptyOutDir: true,
     ignoreDeprecations: true, // Ignore deprecation warnings
     typescript: {
       ignoreDevErrors: true, // Ignore TypeScript errors during development
