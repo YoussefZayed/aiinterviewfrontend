bbbe5d5b6e9843c4e98579b56bff5d993d71db26
YoussefLaunchUp <youssef.Ah.zayed@gmail.com>
2024-06-03 01:17:02 -0500

    chore: Update default port to use environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import path, { join } from "path";
+import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-const srcRoot = join(__dirname, "src");
 
 export default defineConfig({
   plugins: [react()],
@@ -10,19 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  root: srcRoot,
-  base: "./",
-  publicDir: join(__dirname, "public/"),
-  build: {
-    outDir: join(srcRoot, "/out"),
-    emptyOutDir: true,
-    rollupOptions: {},
-  },
   server: {
-    host: "0.0.0.0",
-    port: parseInt(process.env.PORT, 10) || 3000,
-  },
-  optimizeDeps: {
-    exclude: ["path"],
+    port: process.env.PORT || 5173, // Use the PORT environment variable, or default to 3000
   },
 });
